4b2418a84962efe84f61ba40d1320ebbf43cd2b6
franz <fran.one@hotmail.com>
2025-09-02 09:51:24 +0100

    nuevo cambio: ejercicios basicos 1

--- src/proyectfran/Proyectfran.java
+++ src/proyectfran/Proyectfran.java
@@ -6,30 +6,25 @@ import java.util.Scanner;
 
 public class Proyectfran {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
         
-        System.out.println("Hola mundo");    
-        int num1;
-        int num2;      
+        System.out.println("Bienvenido, introduce tu nombre porfavor.");
+        
+        String nombre;
+        int edad;
         
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Ingrese un primer numero: ");
-        num1 = scanner.nextInt();
-        System.out.println("Ingrese un segundo numero");
-        num2 = scanner.nextInt();
-        /*
-        OPERACIONES:
-        */
-        int suma = num1 + num2;
-        double resta = (double)num1 - num2;
-        int multi = num1 * num2;
-        double div = (double)num1 / num2;
         
-        System.out.println("la suma de " + num1 + " y " + num2 + " es: " + suma);
-        System.out.println("la resta de " + num1 + " y " + num2 + " es: " + resta);
-        System.out.println("la multiplicacion de " + num1 + " y " + num2 + " es: " + multi);
-        System.out.println("la division de " + num1 + " y " + num2 + " es: " + div);
-        System.out.println("FIN DEL PROGRAMA");
+        nombre = scanner.nextLine();
+        
+        System.out.println("Genial!, Bienvenido " + nombre + "!");
+        
+        System.out.println("Ahora dime tu edad porfavor.");
+        
+        edad = scanner.nextInt();
+        
+        System.out.println("Genial, " + nombre + ", tienes " + edad + " anios!"); 
+        
         
     } 
 }
